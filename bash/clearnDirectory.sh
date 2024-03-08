d1681fa35c5138bea70ba87c1d206698da29b2e3
Ming <mingz6@outlook.com>
2024-03-08 16:08:30 -0700

    Optimize

--- bash/clearnDirectory.sh
+++ bash/clearnDirectory.sh
@@ -1,5 +1,4 @@
 # chmod +x clearnDirectory.sh 
-dotnet clean
 for i in 1 2; do
-  find . -type d \( -name "bin" -o -name "obj" -o -name "coverage" -o -name "build" -o -name "node_modules" \) -exec rm -r {} \;
+  find . -type d \( -name "bin" -o -name "obj" -o -name "coverage" -o -name "build" -o -name "node_modules" -o -name "dist" \) -exec rm -r {} \;
 done
