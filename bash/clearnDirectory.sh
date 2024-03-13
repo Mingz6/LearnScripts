9348f973c20fcc1866eae75ad4a5f7d64db3cba6
Ming <mingz6@outlook.com>
2024-03-13 13:45:43 -0600

    cache remove

--- bash/clearnDirectory.sh
+++ bash/clearnDirectory.sh
@@ -1,4 +1,4 @@
 # chmod +x clearnDirectory.sh 
 for i in 1 2; do
-  find . -type d \( -name "bin" -o -name "obj" -o -name "coverage" -o -name "build" -o -name "node_modules" -o -name "dist" \) -exec rm -r {} \;
+  find . -type d \( -name "bin" -o -name "obj" -o -name "coverage" -o -name "build" -o -name "node_modules" -o -name "dist" -o -name "cache" \) -exec rm -r {} \;
 done
