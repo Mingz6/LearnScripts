f2fc4cfad36d76dccadd2925fee4d21ec6f00d0c
Ming <mingz6@outlook.com>
2024-03-30 22:22:19 -0600

    New cli to remove

--- bash/clearnDirectory.sh
+++ bash/clearnDirectory.sh
@@ -1,4 +1,6 @@
 # chmod +x clearnDirectory.sh 
 for i in 1 2; do
-  find . -type d \( -name "bin" -o -name "obj" -o -name "coverage" -o -name "build" -o -name "node_modules" -o -name "dist" -o -name "cache" \) -exec rm -r {} \;
+  find . -type d \( -name "bin" -o -name "obj" -o -name "coverage" -o -name "build" -o -name "node_modules" -o -name "dist" -o -name "cache" -o -name "package-lock.json" \) -exec rm -r {} \;
 done
+
+find . -type d \( -name "bin" -o -name "obj" -o -name "coverage" -o -name "build" -o -name "node_modules" -o -name "dist" -o -name "cache" \) -exec rm -rf {} +
