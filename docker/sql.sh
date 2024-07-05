23bdc64744015e5eca453c0ba1c407b0514ee86d
Ming <Mingz6@outlook.com>
2024-07-04 23:43:51 -0600

    Proper documentation

--- docker/sql.sh
+++ docker/sql.sh
@@ -1,4 +1,8 @@
-# Setup the docker volume for the SQL Server 2022 Edmonton instance
+# This script sets up a SQL Server 2022 instance in a Docker container and restores a database from a backup file.
+
+# Step 1: Launch a Docker container for SQL Server 2022
+# This command creates and starts a new Docker container named 'sql2022-edmT' with SQL Server 2022.
+# It sets the SA password, maps port 1433, sets the timezone to America/Edmonton, and mounts a volume for persistent storage.
 sudo docker run -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=<password>' \
    -p 1433:1433 --name sql2022-edmT --hostname sql2022-edmT \
    --platform linux/amd64 \
@@ -8,9 +12,13 @@ sudo docker run -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=<password>' \
    -d \
    mcr.microsoft.com/mssql/server:2022-latest
 
-# In your docker you should then see that there is an sql instance up and running. Now, hit the cog in the top right, go to resources and change Disk image size to at least 60GB, probably 120GB to be sure, hit apply and restart.
-# From there you should be able to create a new database
-# IF that doesn't work, you can hit the script button and compare it to the file bellow. You might have to add WITH REPLACE in order Azure Data Studio to populate the database.
+# Note: After running the container, you should see the SQL instance up and running in Docker.
+# To adjust resources, go to Docker settings > Resources and set the Disk image size to at least 60GB (120GB recommended), then apply and restart.
+
+# Step 2: Restore a Database
+# This SQL command restores a database from a backup file.
+# Replace '<YourDb>' and '<YourDb.bak>' with your database and backup file names, respectively.
+# The 'WITH REPLACE' option is used to overwrite any existing database.
 USE [master]
 RESTORE DATABASE [YourDb] 
 FROM  DISK = N'/<YourDb.bak>' 
@@ -20,16 +28,18 @@ TO N'/var/opt/mssql/data/<YourDb_Data.mdf>',
 MOVE N'YourDb_Log.ldf' 
 TO N'/var/opt/mssql/data/<YourDb_Log.ldf>',  
 NOUNLOAD,  STATS = 5
-# The connection string in your secrets.json should still work, but if it doesn't, you might have to add the port to the connection string like bellow:
+
+# Note: If the connection string in your secrets.json doesn't work, ensure it includes the port as shown below:
 Data Source=localhost,1433;Initial Catalog=YourDb;User Id=sa;Password=<password>;
 
-# Get docker ID
-# e.g. 7edd10a3808d
+# Step 3: Prepare for Database Restoration
+# Retrieve the Docker container ID of the SQL Server instance. e.g. 7edd10a3808d
 docker ps
 
-# Copy BAK file to the docker container
+# Copy the backup file into the Docker container.
+# Replace 'YourDb.BAK' with the name of your backup file and '7edd10a3808d' with your container's ID.
 docker cp YourDb.BAK 7edd10a3808d:/YourDb.BAK
 
-# Restore the BAK file to the Data Studio
-# Delete BAK file from the docker container
+# Step 4: Clean Up
+# After restoring the database, remove the backup file from the container to save space.
 docker exec -u root 7edd10a3808d rm /YourDb.BAK
